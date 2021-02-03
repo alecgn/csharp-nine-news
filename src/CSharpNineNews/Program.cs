f4ea3dc4b1a67512f807e3f69df90cb18865439d
Alessandro Cagliostro <alecgn@hotmail.com>
2021-02-03 20:12:35 -0300

    commit 2020-02-03

---
+++ src/CSharpNineNews/Program.cs
@@ -0,0 +1,45 @@
+﻿using CSharpNineNews.Records;
+using System;
+using System.Text.Json;
+
+namespace CSharpNineNews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StateTax icms12;
+            Console.WriteLine($"Testing Records - Creating new isntance of [{nameof(StateTax)}] named [{nameof(icms12)}]:");
+            icms12 = new()
+            {
+                Initials = "ICMS",
+                Name = "Imposto sobre Circulacao de Mercadorias e Servicos",
+                PercentageRage = 12,
+                StateCode = "SP"
+            };
+            ShowRecordData(icms12);
+
+            StateTax icms08;
+            Console.WriteLine($"\nTesting Records - Creating new instance of [{nameof(StateTax)}] named [{nameof(icms08)}] based on the previous one using \"with\" expression to change [{nameof(StateTax.StateCode)}] and [{nameof(StateTax.PercentageRage)}]:");
+            icms08 = icms12 with { StateCode = "MG", PercentageRage = 8 };
+            ShowRecordData(icms08);
+
+            CityTax iss05;
+            Console.WriteLine($"\nTesting Records - Creating new instance of [{nameof(CityTax)}] named [{nameof(iss05)}]:");
+            iss05 = new("ISS", "Imposto sobre Servico", 5, "Sao Paulo");
+            ShowRecordData(iss05);
+
+            CityTax iss05New;
+            Console.WriteLine($"\nTesting Records - Creating new instance of [{nameof(CityTax)}] named [{nameof(iss05New)}] with exactly the same values as [{nameof(iss05)}]:");
+            iss05New = new("ISS", "Imposto sobre Servico", 5, "Sao Paulo");
+            ShowRecordData(iss05New);
+
+            Console.WriteLine($"\nTesting Equality - Records can compare equality by value, not by reference:");
+            Console.WriteLine($"[{nameof(iss05New)} == {nameof(iss05)}]: {(iss05New == iss05)}");
+        }
+
+        // pretty print with json syntax
+        private static void ShowRecordData(object record) =>
+            Console.WriteLine(JsonSerializer.Serialize(record));
+    }
+}
---
+++ src/CSharpNineNews/Records/Records.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpNineNews.Records
+{
+    public record Tax
+    {
+        public string Initials { get; init; }
+        public string Name { get; init; }
+        public float PercentageRage { get; init; }
+
+        public Tax() { }
+
+        public Tax(string initials, string name, float percentageRate) => 
+            (Initials, Name, PercentageRage) = (initials, name, percentageRate);
+    }
+
+    public record StateTax : Tax
+    {
+        public string StateCode { get; init; }
+    }
+
+    public record CityTax : Tax
+    {
+        public string CityName { get; set; }
+
+        public CityTax(string initials, string name, float percentageRate, string cityName) :
+            base(initials, name, percentageRate) => (CityName) = (cityName);
+    }
+}
